09b4298a615a006eb72ea7398c5d7407d344be71
vikiiv <vikii.ivanova@gmail.com>
2018-03-25 17:32:49 +0300

    hw

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,39 @@
+#a)
+x <- c(4,1,1,4)
+x
+
+
+#b)
+y <- c (1,4)
+
+y
+
+#c)
+x-y
+#As the vector y has shorter lenght(2) than x(4), vector y is recycling.
+# Y is extending to the lenght of x and look like (1,4,1,4) 
+# The result is (3,-3,0, 0) as it is equal to (4-1, 1-4, 1-1, 4-4)
+
+#d)
+s <- c(x,y)
+s
+
+#e)
+
+rep(s,10)
+srep <- rep(s,10) 
+length(srep)
+
+#f)
+rep(s,each=3)
+
+#g)
+#i)
+seq(7,21,by=1)
+#ii)
+7:21
+
+#h)
+lseq <- seq(7,21, by=1)
+length(lseq)
 
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,69 @@
+#a)
+xmin <- c(23, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+
+xmax <- c(25.0,22.8,31.2,27.3,28.4,20.2,24.1)
+
+xmin
+xmax
+
+#b)
+diff <- xmax-xmin
+diff
+
+#c)
+avg_min <- mean(xmin)
+avg_max <- mean(xmax)
+
+avg_min
+avg_max
+
+#d)
+xmin [xmin < avg_min]
+
+#e)
+xmax [xmax > avg_max]
+
+#f)
+names(xmin) <- c('MON', 'TUE', 'WEN','THU', 'FRI', 'SAT','SUN')
+names (xmax) <- c('MON', 'TUE', 'WEN','THU', 'FRI', 'SAT','SUN')
+
+xmin
+xmax
+
+#g)
+
+temperatures <- data.frame(xmin,xmax)
+
+temperatures
+
+
+#h)
+
+temperatures$xminFahrenheit <- xmin*9/5+32
+temperatures
+
+#i)
+
+temperaturesF <- data.frame(
+  minF = xmin*9/5 +32,
+  maxF= xmax*9/5+32
+)
+temperaturesF
+
+#j)
+#i)
+tempMon_Fri<- data.frame(
+  temperaturesF[c(1:5), ]
+)
+
+tempMon_Fri
+
+#ii)
+
+tempMon_Fri <- data.frame(
+temperaturesF[-c(6,7), ]
+)
+
+tempMon_Fri
+
 
